c25cd35948292be46f7e6125d1b4eea75a0808bd
dah007 <dholmberg@gmail.com>
2025-05-13 22:19:52 -0400

    new drive, 'new' files

--- db/additions/user_table.sql
+++ db/additions/user_table.sql
@@ -1,10 +1,11 @@
 -- f1sp.`user` definition
 
 CREATE TABLE `user` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL,
   `name` varchar(100) DEFAULT NULL,
   `email` varchar(100) DEFAULT NULL,
   `passcode` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email_UNIQUE` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+ALTER TABLE user ALTER COLUMN id SET DEFAULT 1;
