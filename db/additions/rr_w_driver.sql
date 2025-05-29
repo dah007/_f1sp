d84dfd0099474f3c3eb93199336c718d1dde49b2
Demian <dholmberg@gmail.com>
2025-05-29 17:05:38 -0400

    feat: Enhance rr_w_driver view to include driver name and country ID

--- db/additions/rr_w_driver.sql
+++ db/additions/rr_w_driver.sql
@@ -2,7 +2,10 @@
 
 create or replace
 algorithm = UNDEFINED view `rr_w_driver` as
-select	d.name, rr.*, r.year
+select	
+		d.name driver_name, 
+		d.country_of_birth_country_id countryId,
+		rr.*, r.year, r.circuit_id
 from	race_result rr
 inner join driver d on rr.driver_id = d.id 
 inner join race r on rr.race_id = r.id
